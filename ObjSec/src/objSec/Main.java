8d033ecdb67f60d2847793db4da96b0fd9990e0a
elt12ddo <elt12ddo@student.lu.se>
2017-09-12 15:56:33 +0200

    Some progress on DH keys

--- ObjSec/src/objSec/Main.java
+++ ObjSec/src/objSec/Main.java
@@ -1,8 +1,28 @@
 package objSec;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+//import org.bouncycastle.crypto.KeyGenerationParameters;
+import org.bouncycastle.crypto.generators.DHKeyPairGenerator;
+import org.bouncycastle.crypto.params.DHKeyGenerationParameters;
+import org.bouncycastle.crypto.params.DHParameters;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		DHKeyPairGenerator gen = new DHKeyPairGenerator();
+		BigInteger p = BigInteger.probablePrime(4096, new Random());
+		BigInteger g = BigInteger.probablePrime(4096, new Random());
+		DHParameters DHparams = new DHParameters(p,g);
+		DHKeyGenerationParameters params = new DHKeyGenerationParameters(new SecureRandom(), DHparams);
+		gen.init(params);
+		AsymmetricCipherKeyPair keyPair = gen.generateKeyPair();
+		
+		System.out.println(keyPair.toString());
 		
 		System.out.println("Hello World!");
 
