e8430c7d180a9e33215626bcaa7fb8ffab6b78d3
elt12ddo <elt12ddo@student.lth.se>
2017-09-13 14:38:07 +0200

    Added a simple udp example found on the webb

--- ObjSec/src/client/Client.java
+++ ObjSec/src/client/Client.java
@@ -1,9 +1,25 @@
 package client;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.DatagramPacket;
+
 public class Client {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, client here");
+	public static void main(String[] args) throws Exception {
+		DatagramSocket socket = new DatagramSocket();
+		int port = 9876;
+		InetAddress host = InetAddress.getByName("localhost");
+		
+		String s = "Some string that is sent over the internet!";
+		
+		DatagramPacket packet = new DatagramPacket(s.getBytes(),s.length(),host,port);
+		socket.send(packet);
+		socket.close();
+		
+		
+		
+		//System.out.println("Hello, client here");
 
 	}
 
--- ObjSec/src/server/Server.java
+++ ObjSec/src/server/Server.java
@@ -1,9 +1,21 @@
 package server;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Server {
 
-	public static void main(String[] args) {
-		System.out.println("I am the Server, feel my drives power");
+	public static void main(String[] args) throws Exception {
+		int port = 9876;
+		DatagramSocket socket = new DatagramSocket(port);
+		byte[] buffer = new byte[1024];
+		DatagramPacket packet = new DatagramPacket(buffer,1024);
+		socket.receive(packet);
+		String s = new String(packet.getData(),0,packet.getLength());
+		System.out.println(s);
+		socket.close();
+		
+		//System.out.println("I am the Server, feel my drives power");
 
 	}
 
