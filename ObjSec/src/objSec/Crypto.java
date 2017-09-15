3ab4430ae6b7f86f3636b76f9f534b38fcbe4103
cart92 <david.cartbo@gmail.com>
2017-09-15 14:28:22 +0200

    Updated Stuff.txt, added a NoKeyException and updated Crypto.java mostly by adding java docs and some other minor changes

--- ObjSec/src/objSec/Crypto.java
+++ ObjSec/src/objSec/Crypto.java
@@ -11,7 +11,9 @@ import java.security.SecureRandom;
 import java.security.Security;
 import java.util.Arrays;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 
@@ -19,19 +21,20 @@ public class Crypto {
 	private Key key;
 	private Cipher cipher;
 	private static final int IV_LENGTH = 16;
-	
-	public Crypto(){
+	/**
+	 * This is the Constructor and it creates the Cipher class used to perform encryption and decryption, if it fail to create the Cipher class it will throw an exception.
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchProviderException
+	 * @throws NoSuchPaddingException
+	 */
+	public Crypto() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException{
 		Security.addProvider(new BouncyCastleProvider());
-		try {
-			cipher = Cipher.getInstance("AES/EAX/NoPadding", "BC");
-		} catch (NoSuchAlgorithmException e) {
-			System.out.println("Something went wrong, Error code 1");
-		} catch (NoSuchProviderException e) {
-			System.out.println("Something went wrong, Error code 2");
-		} catch (NoSuchPaddingException e) {
-			System.out.println("Something went wrong, Error code 3");
-		}
+		cipher = Cipher.getInstance("AES/EAX/NoPadding", "BC");
 	}
+	/**
+	 * This method sets the key that is used by this object to encrypt and  decrypt.
+	 * @param The encryption key
+	 */
 	public void setKey(Key key) {
 		this.key = key;
 	}
@@ -41,23 +44,60 @@ public class Crypto {
 		random.nextBytes(iv);
 		return iv;
 	}
-	public byte[] encrypt(byte[] inData){
+	/**
+	 * This method encrypts the inputed byte array with AES in EAX mode, it also concatenates in the IV used to by the encryption.
+	 * @param The byte array that is to be encrypted.
+	 * @return The concatenated data of the encrypted byte array and the IV needed to decrypt it again.
+	 * @throws NoKeyException
+	 */
+	public byte[] encrypt(byte[] inData) throws NoKeyException{
+		if(key == null) {
+			throw new NoKeyException();
+		}
 		byte[] iv = ivGenerator();
 		try {
 			cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
 		} catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
-			e.printStackTrace();
+			System.out.println("Something went wrong, Error code 11");
+			return null;
 		}
-		return null;
+		byte[] outData = null;
+		try {
+			outData = cipher.doFinal(inData);
+		} catch (IllegalBlockSizeException e) {
+			return null;
+		} catch (BadPaddingException e) {
+			return null;
+		}
+		return Utility.concatByte(outData, iv);
 	}
-	public byte[] decrypt(byte[] inData) {
+	/**
+	 * This method decrypts data that have been encrypted with the encrypt method, it automatically reads the IV from the last bytes of the byte array.
+	 * If the encrypted byte array has been altered or the key is wrong the method will return a Null instead of the decrypted byte array.
+	 * @param The encrypted byte array concatenated with the IV.
+	 * @return The decrypted byte array or a Null if the encrypted data cannot be decrypted.
+	 * @throws NoKeyException
+	 */
+	public byte[] decrypt(byte[] inData) throws NoKeyException {
+		if(key == null) {
+			throw new NoKeyException();
+		}
 		byte[] iv = Arrays.copyOfRange(inData, inData.length - IV_LENGTH, inData.length);
 		inData = Arrays.copyOf(inData, inData.length - IV_LENGTH);
 		try {
 			cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
 		} catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
-			e.printStackTrace();
+			System.out.println("Something went wrong, Error code 21");
+			return null;
 		}
-		return null;
+		byte[] outData = null;
+		try {
+			outData = cipher.doFinal(inData);
+		} catch (IllegalBlockSizeException e) {
+			return null;
+		} catch (BadPaddingException e) {
+			return null;
+		}
+		return outData;
 	}
 }
---
+++ ObjSec/src/objSec/NoKeyException.java
@@ -0,0 +1,9 @@
+package objSec;
+
+public class NoKeyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
