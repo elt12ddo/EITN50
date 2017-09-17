2a93abf50d8db80cb97879e543794ae71746cdb5
cart92 <david.cartbo@gmail.com>
2017-09-17 16:33:33 +0200

    Client is complete enough for testing

--- ObjSec/src/client/Client.java
+++ ObjSec/src/client/Client.java
@@ -2,54 +2,143 @@ package client;
 
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.security.SecureRandom;
 import java.util.Random;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.agreement.DHAgreement;
+import org.bouncycastle.crypto.generators.DHKeyPairGenerator;
+import org.bouncycastle.crypto.params.DHKeyGenerationParameters;
+import org.bouncycastle.crypto.params.DHParameters;
+import org.bouncycastle.crypto.params.DHPrivateKeyParameters;
+import org.bouncycastle.crypto.params.DHPublicKeyParameters;
+import org.bouncycastle.util.Arrays;
+
+import utility.Crypto;
+import utility.MockClientServer;
+import utility.Utility;
+
 import java.math.BigInteger;
 import java.net.DatagramPacket;
 
-public class Client {
+public class Client extends MockClientServer {
+	private final int port = 6789;
+	/*
+	private InetAddress host;
+	private DatagramSocket socket;
+	private BigInteger p;
+	private BigInteger g;
+	private Crypto crypt;*/
 
 	public static void main(String[] args) throws Exception {
-		int port = 6789; //Port the server listens to 
-		DatagramSocket socket = new DatagramSocket(9877);
-		byte[] buffer = new byte[1024];
-		DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
-		InetAddress host = InetAddress.getByName("localhost");
+		new Client().doSetup();
+	}
+
+	private void doSetup() throws Exception {
+		host = InetAddress.getByName("localhost");
+		socket = new DatagramSocket(9877);
+		g = BigInteger.probablePrime(1024, new Random());
+		crypt = new Crypto();
+		//TODO
 		
-		BigInteger g = BigInteger.probablePrime(1024, new Random());
-		DatagramPacket sendPacket = new DatagramPacket(g.toByteArray(),g.toByteArray().length,host,port);
+		
+		doHandshake();
+		return;
+	}
+
+	private void doHandshake() throws Exception {
+		byte[] allReceived;
+		byte[] allSent;
+		byte[] temp;
+		byte[] buffer = new byte[1024];
+		
+		allSent = Utility.concatByte(HELLO, g.toByteArray());
+		
+		// Create and send initial HELLO packet to server with the prime g.
+		DatagramPacket sendPacket = new DatagramPacket(allSent,allSent.length,host,port);
 		socket.send(sendPacket);
 		
+		// Fetch server response, if the flag is not HELLO return.
+		DatagramPacket packet = new DatagramPacket(buffer,1024);
 		socket.receive(packet);
-		System.out.println(packet.getAddress()+", "+packet.getPort()+", \n"+new BigInteger(packet.getData()));
+		if(packet.getData()[0] != HELLO) { return; }
+		allReceived = packet.getData();
 		
+		// Extract the prime p from data.
+		p = new BigInteger(Arrays.copyOfRange(packet.getData(), 1, packet.getLength()));
 		
-		while(true){
-			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Write msg: ");
-			String text = in.readLine();
-			
-			
-			if(text.equals("Exit")){
-				break;
-			}else{
-				buffer = text.getBytes();
-				DatagramPacket out = new DatagramPacket(buffer,buffer.length,host,port);
-				socket.send(out);
-				
-				socket.receive(packet);
-				String msg = "Server says: "+new String(packet.getData(),0,packet.getLength());
-				System.out.println(msg);
-				
-				
-			}
-		}
+		// Initial Diffie-Hellman calculations
+		DHKeyPairGenerator gen = new DHKeyPairGenerator();
+		DHParameters DHparams = new DHParameters(p,g);
+		DHKeyGenerationParameters params = new DHKeyGenerationParameters(new SecureRandom(), DHparams);
+		gen.init(params);
+		AsymmetricCipherKeyPair keyPair = gen.generateKeyPair();
 		
-		socket.close();
+		DHPublicKeyParameters publParams = (DHPublicKeyParameters)keyPair.getPublic();
+		DHPrivateKeyParameters privParams = (DHPrivateKeyParameters)keyPair.getPrivate();
 		
-		System.out.println("Hello, client here");
+		DHAgreement dha = new DHAgreement();
+		dha.init(privParams);
+		BigInteger msg = dha.calculateMessage();
+		
+		// Send msg
+		temp = Utility.concatByte(INIT_DH, msg.toByteArray());
+		allSent = Utility.concatByte(allSent, temp);
+		sendPacket = new DatagramPacket(temp,temp.length,host,port);
+		
+		// Fetch server response to INIT_DH and extract it.
+		socket.receive(packet);
+		if(packet.getData()[0] != INIT_DH) { return; }
+		temp = packet.getData();
+		allReceived = Utility.concatByte(allReceived, temp);
+		BigInteger msgServer = new BigInteger(Arrays.copyOfRange(temp, 1, packet.getData().length));
+		
+		// Send public key to server
+		temp = Utility.concatByte(PUBLIC_KEY, publParams.getY().toByteArray());
+		allSent = Utility.concatByte(allSent, temp);
+		sendPacket = new DatagramPacket(temp,temp.length,host,port);
+		socket.send(sendPacket);
+		
+		// Fetch public key from server
+		socket.receive(packet);
+		allReceived = Utility.concatByte(allReceived, packet.getData());
+		if(packet.getData()[0] != PUBLIC_KEY) { return; }
+		BigInteger serverPubY = new BigInteger(Arrays.copyOfRange(packet.getData(), 1, packet.getLength()));
+		
+		// Calculate shared secret
+		DHPublicKeyParameters serverPublParams = new DHPublicKeyParameters(serverPubY,DHparams);
+		BigInteger key = dha.calculateAgreement(serverPublParams, msgServer);
+		
+		// Create the Crypto object
+		crypt = new Crypto();
+		crypt.setKey(key);
+		
+		// Calculate hash of allReceived and send it
+		SecureRandom random = new SecureRandom();
+		byte[] nonce = new byte[8];
+		random.nextBytes(nonce);
+		temp = crypt.encrypt(Utility.concatByte(calcHash(allReceived),nonce));
+		sendPacket = new DatagramPacket(temp,temp.length,host,port);
+		socket.send(sendPacket);
+		
+		// Fetch response from server and decrypt it
+		socket.receive(packet);
+		temp = crypt.decrypt(packet.getData());
+		
+		// Check nonce
+		if(!Arrays.areEqual(nonce, Arrays.copyOfRange(temp, temp.length - nonce.length, temp.length))) { return; }
+		
+		// Compare hash to server response
+		if(!Arrays.areEqual(calcHash(allSent), Arrays.copyOfRange(temp, 0, temp.length - nonce.length))) { return; }
+		
+		System.out.println("Handshake DONE");
+		doComunication();
+		return;
+	}
 
+	private void doComunication() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
